d5193e403eadf77faa2748313abd221222349a11
Himanshu799 <yourgitemail@example.com>
2022-08-18 02:03:11 +0530

    Upload file

--- static/javascript_of_web_chatbot.js
+++ static/javascript_of_web_chatbot.js
@@ -46,7 +46,9 @@ class Chatbox {
         let msg1 = { name: "User", message: text1 }
         this.messages.push(msg1);
 
-        fetch($SCRIPT_ROOT + '/predict', {
+        
+        // fetch($SCRIPT_ROOT + '/predict', {
+        fetch('http://127.0.0.1:5000/predict', {
             method: 'POST',
             body: JSON.stringify({ message: text1 }),
             mode: 'cors',
