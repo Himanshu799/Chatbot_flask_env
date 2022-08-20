80391e1accbccf0a6a81abef123d630c7e5efa0f
Himanshu799 <90602518+Himanshu799@users.noreply.github.com>
2022-08-20 07:25:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ CORS(app)
 
 # @app.get("/")
 def index_get():
-    return render_template("web_main_2_javas.html")
+    return render_template("index.html")
 
 @app.post("/predict")
 def predict():
